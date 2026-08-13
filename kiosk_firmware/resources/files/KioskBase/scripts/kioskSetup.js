e06dec498c9c31b80fa448b882696265048b1081
igorkll <frerther@gmail.com>
2026-08-13 17:40:05 +0300

    asd

--- kiosk_firmware/resources/files/KioskBase/scripts/kioskSetup.js
+++ kiosk_firmware/resources/files/KioskBase/scripts/kioskSetup.js
@@ -2,13 +2,29 @@
 
 const { ipcRenderer } = require('electron')
 
-function kioskSetup() {
+const overlay = document.getElementById("overlay")
+
+function openKioskSetup() {
     console.log("open kiosk setup")
+    overlay.classList.add("overlay-open")
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            overlay.classList.add("overlay-open2")
+        })
+    })
+}
+
+function closeKioskSetup() {
+    console.log("close kiosk setup")
+    overlay.classList.remove("overlay-open2")
+    setTimeout(() => {
+        overlay.classList.remove("overlay-open")
+    }, 1000)
 }
 
 ipcRenderer.on('open-kiosk-setup', (event, data) => {
     console.log("open kiosk setup event")
-    kioskSetup()
+    openKioskSetup()
 });
 
 setTimeout(() => {
