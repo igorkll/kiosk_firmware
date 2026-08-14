32860c3e35ba346895eed2b57098425cd87cc17c
igorkll <frerther@gmail.com>
2026-08-14 16:40:32 +0300

    Update prepair.sh

--- kiosk_firmware/resources/chroot/prepair.sh
+++ kiosk_firmware/resources/chroot/prepair.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+mkdir -p /etc/acpi/events
 cat > "/etc/acpi/events/powerbtn-custom" <<EOF
 event=button[ /]power
 action=/KioskBase/open_kiosk_setup.sh
 EOF
+chmod 644 /etc/acpi/events/powerbtn-custom
 
 touch /.kiosk_firmware
 
