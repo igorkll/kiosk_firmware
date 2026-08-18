a9812947419e6f8aee705dee9bddda66b92d6e1b
igorkll <frerther@gmail.com>
2026-08-18 23:33:12 +0300

    qwe

--- kiosk_firmware/resources/files/KioskBase/slider/slider.js
+++ kiosk_firmware/resources/files/KioskBase/slider/slider.js
@@ -4,6 +4,12 @@ window.createSlider = function(value) {
     let slider = document.createElement("div")
     slider.classList.add("slider")
 
+    let sliderBody = document.createElement("div")
+    sliderBody.classList.add("slider-body")
+
+    
+    slider.appendChild(sliderBody)
+
 
     return slider
 }
