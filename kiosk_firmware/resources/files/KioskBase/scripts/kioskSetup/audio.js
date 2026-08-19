2397750679069a6920bc383b75c49d7c124e7359
igorkll <frerther@gmail.com>
2026-08-19 20:09:42 +0300

    Update audio.js

--- kiosk_firmware/resources/files/KioskBase/scripts/kioskSetup/audio.js
+++ kiosk_firmware/resources/files/KioskBase/scripts/kioskSetup/audio.js
@@ -2,18 +2,18 @@
 
 const tab = tab_create()
 
-const setOutputVolumeReduced = reduceNumberOfCalls(audio_setOutputVolume)
-const setInputVolumeReduced = reduceNumberOfCalls(audio_setInputVolume)
+const setOutputVolumeReduced = reduceNumberOfCalls(audio_setOutputVolumeAsync)
+const setInputVolumeReduced = reduceNumberOfCalls(audio_setInputVolumeAsync)
 
 // -------------------------- 
 
 const slider_outputVolume = createSlider()
 slider_outputVolume.addEventListener("slide", detailWrap(setOutputVolumeReduced))
-slider_outputVolume.addEventListener("slideEnd", detailWrap(audio_setOutputVolume))
+slider_outputVolume.addEventListener("slideEnd", detailWrap(audio_setOutputVolumeAsync))
 
 const slider_inputVolume = createSlider()
 slider_inputVolume.addEventListener("slide", detailWrap(setInputVolumeReduced))
-slider_inputVolume.addEventListener("slideEnd", detailWrap(audio_setInputVolume))
+slider_inputVolume.addEventListener("slideEnd", detailWrap(audio_setInputVolumeAsync))
 
 // -------------------------- 
 
